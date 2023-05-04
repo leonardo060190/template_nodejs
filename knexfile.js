e196dcacf008a11f5c19469ae6143894eb1bf7d4
leonardo060190 <leonardo-morruga@hotmail.com>
2023-05-03 21:53:00 -0300

    instalado o knex e o sqlite3

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    //coloca o banco de dados a ser utilizado
+    client: 'sqlite3',
+    connection: {
+      filename: './data/editora.db3'
+    },
+
+    useNullAsDefault: true,
+    migrations:{
+      directory: './data/migrations'
+    },
+
+    seeds:{
+      directory: './data/seeds'
+    }
+  },
+};
